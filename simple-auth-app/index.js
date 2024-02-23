23514ddff0c45c2cb080b7bcb306511120d36157
Gajanan Mallik <gajanan.mallik@tdcx.com>
2024-02-23 13:16:21 +0530

    check accout route added

--- simple-auth-app/index.js
+++ simple-auth-app/index.js
@@ -23,6 +23,43 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use("/users", userRoutes);
 
+app.get("/", function (request, response) {
+  console.log("This is main route!!");
+  response.send("This is main page 1!!");
+});
+
+app.get("/check-account", function (req, res) {
+  //check for account number is given
+  const accountNumber = req.query.accountNumber; //request body
+
+  const accountBalances = [
+    {
+      id: 1,
+      account_number: 1001,
+      customer_name: "Sushree",
+      balance: 10000000,
+    },
+    { id: 2, account_number: 1002, customer_name: "Gajanan", balance: 500 },
+    { id: 3, account_number: 1003, customer_name: "Mummun", balance: 23400 },
+  ];
+
+  let data = [];
+
+  for (const item of accountBalances) {
+    if (item.account_number === parseInt(accountNumber)) {
+      data.push(item);
+      break;
+    }
+  }
+
+  //resonse body
+  return res.send(
+    data && data.length > 0
+      ? data
+      : "No customer exists with this account number!!"
+  );
+});
+
 //listen the server
 if (connection.config.connectionConfig.host) {
   app.listen(port, function () {
